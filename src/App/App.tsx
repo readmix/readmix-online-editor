03de63c468bda2cf1019060c1223ed9d0872f61d
Ankur Seth <iankurseth@gmail.com>
2023-04-28 20:12:56 +0530

    Integrated app with apiCommand

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,18 @@
+import React from 'react';
+
 import 'miniwindcss';
 import './App.css';
 
 
 
 export default function App () {
+	React.useEffect(() => {
+		fetch("http://127.0.0.1:1997/")
+			.then(res => res.json())
+			.then(data => {
+				console.log(data);
+			});
+	}, []);
 
 	return (
 		<div className="min-h-screen bg-red-500 text-white flex">
